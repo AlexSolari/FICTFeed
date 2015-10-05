4955759df887894f69588f70ffd00c61c46efee2
AlexSolari <alexsanify@gmail.com>
2015-10-05 10:58:22 +0300

    Created basic DependencyResolver functionality

---
+++ Resolver/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependecyResolver
+{
+    public static class Resolver
+    {
+        static Dictionary<Type, Type> typeDependencies = new Dictionary<Type, Type>();
+
+        public static Type Resolve<T>()
+        {
+            return typeDependencies[typeof(T)];
+        }
+
+        public static void RegisterType<T,I>()
+        {
+            typeDependencies.Add(typeof(T), typeof(I));
+        }
+
+        public static T GetInstance<T>()
+        {
+            return (T)Activator.CreateInstance(Resolve<T>());
+        }
+    }
+}
