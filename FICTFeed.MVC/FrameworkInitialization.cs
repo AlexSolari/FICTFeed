ccf8ca7ed2f27622e4a9e4148b825ad21e9e8ed8
AlexSolari @ PC <gearbots@yandex.com>
2015-10-10 20:28:49 +0300

    Added framework initialization

---
+++ FICTFeed.MVC/FrameworkInitialization.cs
@@ -0,0 +1,17 @@
+﻿using FICTFeed.DependecyResolver;
+using FICTFeed.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FICTFeed.MVC
+{
+    public static class FrameworkInitializer
+    {
+        public static void Start()
+        {
+            Resolver.RegisterSingleton<Encryptor>(new Encryptor());
+        }
+    }
+}
--- FICTFeed.MVC/Global.asax.cs
+++ FICTFeed.MVC/Global.asax.cs
@@ -18,6 +18,7 @@ namespace FICTFeed.MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FrameworkInitializer.Start();
         }
     }
 }
