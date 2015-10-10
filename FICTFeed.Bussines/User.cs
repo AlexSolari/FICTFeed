70470167f66704d40669c272da56fb218e138036
AlexSolari @ PC <gearbots@yandex.com>
2015-10-10 17:30:59 +0300

    Added base User entity

---
+++ FICTFeed.Bussines/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FICTFeed.Bussines
+{
+    class User : Entity
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string PasswordCrypted { get; set; }
+
+        public virtual string Mail { get; set; }
+    }
+}
