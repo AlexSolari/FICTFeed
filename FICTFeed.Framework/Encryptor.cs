a98cbb58233d96563c22c6a5c51cdf29414a77ae
AlexSolari @ PC <gearbots@yandex.com>
2015-10-10 20:21:16 +0300

    Framework project w/ crypt class added

---
+++ FICTFeed.Framework/Encryptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FICTFeed.Framework
+{
+    public class Encryptor
+    {
+        private const string salt = "ttKjZwmYXhQNGxGI2wAU_lFkaJDYCoYJyu1xVTmIp";
+        public Encryptor()
+        {
+
+        }
+
+        public string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            var result = md5.Hash;
+
+            var strBuilder = new StringBuilder();
+            foreach (var item in result)
+            {
+                strBuilder.Append(item.ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
+        public string CryptPassword(string password)
+        {
+            return MD5Hash(password + salt);
+        }
+    }
+}
