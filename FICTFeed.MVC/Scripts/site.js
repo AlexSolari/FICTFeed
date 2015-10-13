878b561e6b7028efb38e15586d7fac21c3ad2282
AlexSolari <alexsanify@gmail.com>
2015-10-13 22:39:32 +0300

    Redesign started

--- FICTFeed.MVC/Scripts/site.js
+++ FICTFeed.MVC/Scripts/site.js
@@ -1,3 +1,8 @@
 ﻿$(window).load(function () {
-
+    $("#navigation-button").click(function () {
+        $("#dropdown-navigation").toggleClass("hidden");
+    });
+    $("*:not(*:has(#dropdown-navigation), #search, #navigation-button)").click(function () {
+        $("#dropdown-navigation").addClass("hidden");
+    });
 });
