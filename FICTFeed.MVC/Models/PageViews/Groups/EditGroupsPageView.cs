3e326e79f0f795ec22b04c8ddb567d59ad5c6f9a
AlexSolari <alexsanify@gmail.com>
2015-12-22 20:29:24 +0200

    Removed stub with functionality on groups list

--- FICTFeed.MVC/Models/PageViews/Groups/EditGroupsPageView.cs
+++ FICTFeed.MVC/Models/PageViews/Groups/EditGroupsPageView.cs
@@ -1,4 +1,6 @@
-﻿using FICTFeed.MVC.Models.ViewModels.Groups;
+﻿using FICTFeed.DependecyResolver;
+using FICTFeed.Framework.Users;
+using FICTFeed.MVC.Models.ViewModels.Groups;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,29 @@ namespace FICTFeed.MVC.Models.PageViews.Groups
     {
         public IEnumerable<GroupEditViewModel> Groups { get; set; }
 
+        public Dictionary<Guid, IEnumerable<Bussines.Models.User>> Users { get; set; }
+
         public EditGroupsPageView(IEnumerable<GroupEditViewModel> groups)
             : base()
         {
             Groups = groups;
+
+            Users = new Dictionary<Guid, IEnumerable<Bussines.Models.User>>();
+
+            var userManager = Resolver.GetInstance<IUserManager>();
+
+            var users = userManager.GetList();
+
+            foreach (var item in groups)
+            {
+                Users.Add(item.Id, users.Where(x=>x.GroupId == item.Id));
+            }
         }
 
         public EditGroupsPageView()
         {
             Groups = new List<GroupEditViewModel>();
+            Users = new Dictionary<Guid, IEnumerable<Bussines.Models.User>>();
         }
     }
 }
